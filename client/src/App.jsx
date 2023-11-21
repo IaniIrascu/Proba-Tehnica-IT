2817b46188ac435b85226393ff2703f6c3e3975f
IaniIrascu <iirascu@gmail.com>
2023-11-21 17:44:25 +0200

    test site

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,71 @@
-import './App.css';
-import { useState, useEffect } from "react";
+import "./App.css";
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import { nanoid } from "nanoid";
 
+function App(props) {
 
-//functions
-import { getTest } from './functions/test';
+  const[tasks, setTasks] = useState(props.tasks);
 
-function App() {
-  const [data, setData] = useState("Hello World!");
-  useEffect(() => {
-    getTest()
-        .then((res) => {
-            setData(res.message);
-        })
-        .catch(err => console.log(err));
-  }, []);
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if(id === task.id)
+        return { ...task, completed: !task.completed };
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if(id === task.id)
+        return{ ...task, name: newName};
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks.map((task) => (
+  <Todo 
+  id={task.id} 
+  name={task.name} 
+  completed={task.completed} 
+  key={task.id}
+  deleteTask={deleteTask}
+  />
+  ));
+
+  const tasksNoun = taskList.length !==1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
-    <div className="App">
-      <h1>(data)</h1>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
